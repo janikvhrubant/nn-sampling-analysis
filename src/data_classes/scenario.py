321be88d1958a038dbe448d34c73980a54f14e29
Janik <108046501+janikvhrubant@users.noreply.github.com>
2025-04-11 19:53:05 +0200

    refactor

--- src/data_classes/scenario.py
+++ src/data_classes/scenario.py
@@ -9,7 +9,8 @@ class ScenarioSettings:
     OUTPUT_DIM: int
     SCENARIO: Scenario
     DATA_PATH: str
-    TRINING_SET_SIZES: list
+    TRAINING_SET_SIZES: list
+    COL_NAMES: list[str]
 
     def __init__(self, scenario: Scenario):
         self.SCENARIO = scenario
@@ -19,18 +20,20 @@ class ScenarioSettings:
             case Scenario.BSPDE:
                 self.INPUT_DIM = 5
                 self.OUTPUT_DIM = 1
-                self.TRINING_SET_SIZES = (np.logspace(5, 11, 7, base=2).astype(int)).tolist()
+                self.TRAINING_SET_SIZES = (np.logspace(5, 11, 7, base=2).astype(int)).tolist()
             case Scenario.SUM_SINES:
                 self.INPUT_DIM = 6
                 self.OUTPUT_DIM = 1
-                self.TRINING_SET_SIZES = (np.logspace(7, 13, 7, base=2).astype(int)).tolist()
+                self.TRAINING_SET_SIZES = (np.logspace(7, 13, 7, base=2).astype(int)).tolist()
+                self.COL_NAMES = ['dim_1', 'dim_2', 'dim_3', 'dim_4', 'dim_5', 'dim_6', 'sum_sines']
             case Scenario.PROJECTILE:
-                self.INPUT_DIM = 3
+                self.INPUT_DIM = 7
                 self.OUTPUT_DIM = 1
-                self.TRINING_SET_SIZES = (np.logspace(4, 10, 7, base=2).astype(int)).tolist()
+                self.TRAINING_SET_SIZES = (np.logspace(4, 10, 7, base=2).astype(int)).tolist()
+                self.COL_NAMES = ['density', 'radius', 'drag_coeff', 'mass', 'height', 'alpha', 'velocity', 'distance']
             case Scenario.AIRFOIL:
                 self.INPUT_DIM = 4
                 self.OUTPUT_DIM = 1
-                self.TRINING_SET_SIZES = (np.logspace(2, 8, 7, base=2).astype(int)).tolist()
+                self.TRAINING_SET_SIZES = (np.logspace(2, 8, 7, base=2).astype(int)).tolist()
             case _:
                 raise ValueError(f"Unknown scenario: {scenario}")
