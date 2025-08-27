f06b1271328e8e22554eda83e2a73467b36512f9
Hrubant <janik.hrubant@sap.com>
2025-08-27 09:04:05 +0200

    wip

--- notebooks/experiment.py
+++ notebooks/experiment.py
@@ -16,7 +16,7 @@ from data_classes.training_config import AdamTrainingConfig, TrainingSettings, L
 from models import SequentialNeuralNetwork
 
 experiment = Experiment(
-    SAMPLING_METHOD=SamplingMethod.MC,
+    SAMPLING_METHOD=SamplingMethod.SOBOL,
     SCENARIO=Scenario.SUM_SINES_6D
 )
 
@@ -27,7 +27,7 @@ input_data = InputData(scenario_settings.DATA_PATH)
 
 # NN Architectures
 widths = [4,6,8]
-depths = [2,4,8]
+depths = [4,8,16]
 activation_functions = [torch.nn.Sigmoid, torch.nn.Tanh]
 
 # Optimizer settings
@@ -46,7 +46,7 @@ lion_lr_wd_combos = [(3e-3,0),(3e-3, 1e-6),(1e-3,0),(1e-3,1e-6),(3e-3,1e-5),(1e-
 # Learing settings:
 training_set_sizes = [128,512,1024,2048,4096,8192]
 batch_sizes = [64,256,1024]
-epochs = [500,1000,1500]
+epochs = [800,1200]
 
 all_training_settings = []
 for width, depth, activation_func, train_set_size, batch_size, epoch in product(widths, depths, activation_functions, training_set_sizes, batch_sizes, epochs):
