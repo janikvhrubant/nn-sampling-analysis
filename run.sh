e12620e1405b34af72bf184245b5bb1fa8dce4d1
Hrubant <janik.hrubant@sap.com>
2025-08-30 23:40:12 +0200

    wip

--- run.sh
+++ run.sh
@@ -4,23 +4,23 @@ python notebooks/lion_tuning.py --scenario projectile --sampling sobol && \
 python notebooks/adam_tuning.py --scenario projectile --sampling sobol && \
 python notebooks/lion_tuning.py --scenario projectile --sampling mc && \
 python notebooks/adam_tuning.py --scenario projectile --sampling mc && \
-python notebooks/lion_tuning.py --scenario simsin6d --sampling halton && \
-python notebooks/adam_tuning.py --scenario simsin6d --sampling halton && \
-python notebooks/lion_tuning.py --scenario simsin6d --sampling sobol && \
-python notebooks/adam_tuning.py --scenario simsin6d --sampling sobol && \
-python notebooks/lion_tuning.py --scenario simsin6d --sampling mc && \
-python notebooks/adam_tuning.py --scenario simsin6d --sampling mc
+python notebooks/lion_tuning.py --scenario sumsin6d --sampling halton && \
+python notebooks/adam_tuning.py --scenario sumsin6d --sampling halton && \
+python notebooks/lion_tuning.py --scenario sumsin6d --sampling sobol && \
+python notebooks/adam_tuning.py --scenario sumsin6d --sampling sobol && \
+python notebooks/lion_tuning.py --scenario sumsin6d --sampling mc && \
+python notebooks/adam_tuning.py --scenario sumsin6d --sampling mc
 
-# python notebooks/lion_tuning.py --scenario simsin8d --sampling halton && \
-# python notebooks/adam_tuning.py --scenario simsin8d --sampling halton && \
-# python notebooks/lion_tuning.py --scenario simsin8d --sampling sobol && \
-# python notebooks/adam_tuning.py --scenario simsin8d --sampling sobol && \
-# python notebooks/lion_tuning.py --scenario simsin8d --sampling mc && \
-# python notebooks/adam_tuning.py --scenario simsin8d --sampling mc && \
+# python notebooks/lion_tuning.py --scenario sumsin8d --sampling halton && \
+# python notebooks/adam_tuning.py --scenario sumsin8d --sampling halton && \
+# python notebooks/lion_tuning.py --scenario sumsin8d --sampling sobol && \
+# python notebooks/adam_tuning.py --scenario sumsin8d --sampling sobol && \
+# python notebooks/lion_tuning.py --scenario sumsin8d --sampling mc && \
+# python notebooks/adam_tuning.py --scenario sumsin8d --sampling mc && \
 
-# python notebooks/lion_tuning.py --scenario simsin10d --sampling halton && \
-# python notebooks/adam_tuning.py --scenario simsin10d --sampling halton && \
-# python notebooks/lion_tuning.py --scenario simsin10d --sampling sobol && \
-# python notebooks/adam_tuning.py --scenario simsin10d --sampling sobol && \
-# python notebooks/lion_tuning.py --scenario simsin10d --sampling mc && \
-# python notebooks/adam_tuning.py --scenario simsin10d --sampling mc
+# python notebooks/lion_tuning.py --scenario sumsin10d --sampling halton && \
+# python notebooks/adam_tuning.py --scenario sumsin10d --sampling halton && \
+# python notebooks/lion_tuning.py --scenario sumsin10d --sampling sobol && \
+# python notebooks/adam_tuning.py --scenario sumsin10d --sampling sobol && \
+# python notebooks/lion_tuning.py --scenario sumsin10d --sampling mc && \
+# python notebooks/adam_tuning.py --scenario sumsin10d --sampling mc
